0d29e43760cbe80a4b9cf4388104477114e25e9a
flo <flosantiagoespiritu@outlook.com>
2026-05-16 20:03:56 +0800

    Digital business card

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h3 className="about--title">About</h3>
+      <p className="about--paragraph">
+        Practicing Web Development to become a Junior Web Developer
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Interests() {
+  return (
+    <div className="interests">
+      <h3 className="interests--title">Interests</h3>
+      <p className="interests--paragraph">
+       Music Lover. Percussion Drum Player. Tutorial Addicts. Instant Coffee Drinker.('-')
+      </p>
+    </div>
+  );
+}
